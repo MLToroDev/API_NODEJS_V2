48d45037620e186a390be8b064968fbef0156eab
ELSECAFEMINAS <realmateox22@gmil.com>
2022-10-20 10:42:11 -0500

    Se ajusta el metodo get para retornar al front

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,10 @@ import ProductoRouter from './routes/Producto.router';
 
 const app = express();
 const PORT = 3000;
-var cors = require('cors')
-
-app.use(cors()) // Use this after the variable declaration
-
 app.use(express.json());
 
-app.use('/api/producto', ProductoRouter);
 
+app.use('/api/producto', ProductoRouter);
 
 
 app.listen(PORT, () => {
